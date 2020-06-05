2e47f11ab546b084bac869ef5f9114bb469da0a8
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2020-06-05 07:41:25 +0100

    Update CampsController.cs

--- TheCodeCamp/Controllers/CampsController.cs
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -25,7 +25,7 @@ namespace TheCodeCamp.Controllers
             _mapper = mapper;
         }
 
-        //get all camps route
+        //changing from github merge conflict test
         [Route()]
         public async Task<IHttpActionResult> Get(bool includeTalks = false)
         {
